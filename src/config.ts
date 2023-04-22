c99f128a3aecce3dcbd23f1e1dec82b2b59b620f
Data88 <andrew1on1@gmail.com>
2023-04-22 09:08:14 +0200

    config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://53c.co.za",
+  author: "Andrew",
+  desc: "Be connected. Stay protected.",
+  title: "53C",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
